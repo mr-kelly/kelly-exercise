ec5bbbe832c944f72bc68a8674274543b54ebfad
Mrkelly <chepy.v@gmail.com>
2014-09-29 13:11:49 +0800

    profile

--- star_catcher/database_client.py
+++ star_catcher/database_client.py
@@ -1,6 +1,8 @@
 import scrapy
 import sqlite3
 
+
+
 class DatabaseClient:
 	def __init__(self, path):
 		self.db = sqlite3.connect(path)
@@ -8,5 +10,9 @@ class DatabaseClient:
 	def execute(self, *args):
 		return self.client.execute(*args)
 
+	def replace_into(self, table, cols, values):
+		self.client.execute("REPLACE into `%s` (%s) VALUES (%s);" % (table, cols, values))
+		self.db.commit()
+
+
 db_client = DatabaseClient("./data.db")
-db_client.execute("create table catalog (id integer primary key,pid integer,name varchar(10) UNIQUE,nickname text NULL)")
--- star_catcher/star/spiders/star.py
+++ star_catcher/star/spiders/star.py
@@ -2,6 +2,9 @@
 import scrapy
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors import LinkExtractor
+import re, sys
+sys.path.append("..")
+import database_client
 
 class StarSpider(CrawlSpider):
 	name = "star"
@@ -12,14 +15,23 @@ class StarSpider(CrawlSpider):
 	]
 
 	rules = (
-		Rule(LinkExtractor(allow=('(.*)\.html')), follow=True, callback='parse_item'),
+		Rule(LinkExtractor(allow=('(.*)/fanhao/(.*)\.html')), follow=True, callback='parse_item'),
 	)
 
 	def parse_item(self, response):
-		sels = response.xpath('//div[@class="article_img_mid"]/h1/text()')
-		if len(sels) > 0:
+		title_sels = response.xpath('//div[@class="article_img_mid"]')
+		
+		if len(title_sels) > 0:
+			main_title_sel = title_sels[0]
+
 			print "catch url: %s" % response.url
-			star_name = sels[0].extract()
-			print star_name
+			star_id = re.match('.*/(\d+).html', response.url).group(1)
+			star_name = main_title_sel.xpath('h1/text()')[0].extract()
+			print "%s---%s" % (star_id, star_name)
+
+			database_client.db_client.replace_into(
+				'star', 
+				'id, name', 
+				'%s, "%s"' % (star_id, star_name))
 
 		yield None
