5f181cb8f7c5dc2ddcf693daa68a82ae2f57de86
mrkelly <chepy.v@gmail.com>
2014-10-07 22:31:21 +0800

    data

--- star_catcher/spider_project/database_client.py
+++ star_catcher/spider_project/database_client.py
@@ -1,18 +1,41 @@
+#coding=utf-8
 import scrapy
 import sqlite3
-
-
 
 class DatabaseClient:
 	def __init__(self, path):
 		self.db = sqlite3.connect(path)
 		self.client = self.db.cursor() # main cursor
+		self.client.execute("""
+		CREATE TABLE IF NOT EXISTS "star" (
+		"id"  integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+		"name"  TEXT,
+		"small_pic_url"  TEXT,
+		"big_pic_url"  TEXT,
+		"content"  TEXT
+		) ;
+
+		""")
+		self.client.execute("""
+		CREATE TABLE IF NOT EXISTS "movie" (
+		"id"  TEXT NOT NULL,
+		"star_id"  integer,
+		"name"  TEXT,
+		"movie_length"  TEXT,
+		"public_time"  TEXT,
+		"other"  TEXT,
+		PRIMARY KEY ("id" ASC)
+		);
+		""")
+
 	def execute(self, *args):
 		return self.client.execute(*args)
 
 	def replace_into(self, table, cols, values):
-		self.client.execute("REPLACE into `%s` (%s) VALUES (%s);" % (table, cols, values))
+		sql = "REPLACE into `%s` (%s) VALUES (%s);" % (table, cols, values)
+		print 'Execute SQL: %s' % sql
+		self.client.execute(sql)
 		self.db.commit()
 
 
-db_client = DatabaseClient("./data.db")
+db_client = DatabaseClient("/home/kelly/data.db")
--- star_catcher/spider_project/star/spiders/star.py
+++ star_catcher/spider_project/star/spiders/star.py
@@ -2,9 +2,10 @@
 import scrapy
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors import LinkExtractor
-import re, sys
-sys.path.append("..")
+import re
 from database_client import *
+import os
+
 
 class StarSpider(CrawlSpider):
 	name = "star"
@@ -31,35 +32,51 @@ class StarSpider(CrawlSpider):
 			star_name = main_title_sel.xpath('h1/text()')[0].extract()
 			print "%s---%s" % (star_id, star_name)
 
+			# 小图片地址
+
+			small_pic_url = ""
+			small_pic_xpath = response.xpath('//div[@class="article_img_left"]/img/@src')
+			if len(small_pic_xpath) > 0:
+				small_pic_url = "http://%s%s" % (self.allowed_domains[0], small_pic_xpath[0].extract())
+			print u'小图片地址: %s' % small_pic_url
+
+			# 大图片地址
+			big_pic_url = ""
+			big_pic_xpath = response.xpath('//div[@class="arcBody"]/div[@id="disappear"]//img/@src')
+			if len(big_pic_xpath) > 0:
+				big_pic_url = "http://%s%s" % (self.allowed_domains[0], big_pic_xpath[0].extract())
+			print u'大图地址: %s' % big_pic_url
+
+			# 文字内容
+			# content = response.xpath('//div[@class="arcBody"]/div[@id="disappear"]')[0].extract()
+			# print content
+
+			# 表格开始
+			fh_table_rows = response.xpath('//div[@class="arcBody"]/table/tbody/tr')
+			if (len(fh_table_rows) > 0):
+				for row in fh_table_rows: # tr
+					cols = row.xpath('td/text()') # td
+					if len(cols) == 5:
+						# print u'番号列表表格匹配到'
+						fanhao = cols[0].extract().strip()
+						av_name = cols[1].extract().strip()
+						av_time = cols[2].extract().strip()
+						av_public = cols[3].extract().strip()
+						av_other = cols[4].extract().strip()
+						print fanhao, av_name, av_time, av_public, av_other
+						db_client.replace_into(
+							'movie', 
+							'id, star_id, name, movie_length, public_time, other', 
+							'"%s", %s, "%s", "%s", "%s", "%s"' % (fanhao, star_id, av_name, av_time, av_public, av_other))		
+					else:
+						print u'表格不是5列，而是%d列, 非番号列表吧' % len(cols)
+						
+
 			db_client.replace_into(
 				'star', 
-				'id, name', 
-				'%s, "%s"' % (star_id, star_name))
+				'id, name, small_pic_url, big_pic_url', 
+				'%s, "%s", "%s", "%s"' % (star_id, star_name, small_pic_url, big_pic_url))
 
-
-		# 小图片地址
-		pic_url = response.xpath('//div[@class="article_img_left"]/img/@src').extract()
-		print u'小图片地址: %s' % pic_url
-
-		# 大图片地址
-		big_pic_url = response.xpath('//div[@class="arcBody"]/div[@id="disappear"]//img/@src').extract()
-		print u'大图地址: %s' %big_pic_url
-
-		# 文字内容
-		content = response.xpath('//div[@class="arcBody"]/div[@id="disappear"]').extract()
-
-		# 表格开始
-		fh_table_rows = response.xpath('//div[@class="arcBody"]/table/tbody/tr')
-		if (len(fh_table_rows) > 0):
-			for row in fh_table_rows: # tr
-				cols = row.xpath('td/text()') # td
-				assert(len(cols) == 5)
-				for col in cols:
-					print 'Col-Value: %s' % col.extract().strip()
-				# response.xpath('//table[@class="fanhao_list_table"]/tbody/tr')[0].extract()
-
-
-
-		
+			
 
 		yield None
